b1e4ce538e2bd1b3b47ab0bc540ddb63507fee51
Alang315 <alguienxc2@hotmail.com>
2024-11-22 09:07:40 -0600

    Method with each iteration tableu and result operation

--- NWCM.py
+++ NWCM.py
@@ -5,35 +5,39 @@ class NWCM:
         self.supply = supply
         self.allocation = self.northwest_corner_method(self.cost_matrix, self.supply, self.demand)
         self.result = self.calculate_total_cost(self.allocation, self.cost_matrix)
-        self.printTableusNresults(self.allocation, self.result)
+        self.printTableus(self.allocation)
+        self.printResults(self.result)
 
 
     def northwest_corner_method(self, cost_matrix, supply, demand):
+        iteration = 0
         rows = len(supply)
         cols = len(demand)
         allocation = [[0] * cols for _ in range(rows)]
-        tempallocaion = []
         
         i, j = 0, 0  # Start at the top-left corner
         
         while i < rows and j < cols:
             # Allocate the minimum of supply and demand
             allocation[i][j] = min(supply[i], demand[j])
-            
+            self.printTableus(allocation, iteration)
             # Update supply and demand
             if supply[i] < demand[j]:
                 demand[j] -= supply[i]
                 supply[i] = 0
                 i += 1  # Move to the next source
+                iteration += 1
             elif supply[i] > demand[j]:
                 supply[i] -= demand[j]
                 demand[j] = 0
                 j += 1  # Move to the next destination
+                iteration += 1
             else:  # supply[i] == demand[j]
                 supply[i] = 0
                 demand[j] = 0
                 i += 1
                 j += 1  # Move diagonally to the next source and destination
+                iteration += 1
         return allocation
 
 
@@ -41,19 +45,29 @@ class NWCM:
         total_cost = 0
         rows = len(allocation)
         cols = len(allocation[0])
-        
+        totalCostOperation = str("")
+
         for i in range(rows):
             for j in range(cols):
                 total_cost += allocation[i][j] * cost_matrix[i][j]
-        
-        return total_cost
+                if(allocation[i][j] != 0 and cost_matrix[i][j] !=0):
+                    totalCostOperation += str(allocation[i][j]) +"*" + str(cost_matrix[i][j])
+                    if( i < rows-1 or j < cols-1):
+                        totalCostOperation += " + "
+
+
+        return totalCostOperation + " = " + str(total_cost)
     
     
-    def printTableusNresults(self, tableu, result):
+    def printTableus(self, tableu, iteration = "Final Tableu:"):
+        print("iteration #" ,iteration)
         for row in tableu:
             print(row)
-
+        print("\n")
+    
+    def printResults(self, result):
         print("Total transportation Cost:", result)
+
 
 
 
